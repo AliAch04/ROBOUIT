b1d83d752fb0ae86c2003fd6f251b1f6c32871a7
Hajar1485 <145665235+Hajar1485@users.noreply.github.com>
2025-11-27 23:01:33 +0100

    Delete ROBOUIT-HAJAR directory

--- ROBOUIT-HAJAR/FileManager.hpp
+++
@@ -1,9 +0,0 @@
-// FileManager.hpp
-#pragma once
-#include <vector>
-#include <string>
-
-class FileManager {
-public:
-    static bool readLines(const std::string& path, std::vector<std::string>& out);
-};
